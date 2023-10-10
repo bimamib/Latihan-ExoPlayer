c28aa49065786a400dd4a720677de96318a6195c
bimamib <bimaprast1601@gmail.com>
2023-10-10 23:31:29 +0700

    memindahkan ExoPlayer yang berada di kelas MainActivity ke dalam PlaybackService

--- app/src/main/java/com/bima/myexoplayer/PlaybackService.kt
+++ app/src/main/java/com/bima/myexoplayer/PlaybackService.kt
@@ -1,9 +1,28 @@
 package com.bima.myexoplayer
 
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
 
 class PlaybackService : MediaSessionService() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initializeSessionAndPlayer()
+    }
+
+    private fun initializeSessionAndPlayer() {
+
+        val videoItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
+        val audioItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3")
+
+        val player = ExoPlayer.Builder(this).build().also { exoPlayer ->
+            exoPlayer.setMediaItem(videoItem)
+            exoPlayer.addMediaItem(audioItem)
+            exoPlayer.prepare()
+        }
+    }
 
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
 
